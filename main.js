2e69fac1b793d8d3f9d9e00e58a4539876f906ff
dave <d.gillespie221@gmail.com>
2019-03-18 16:25:58 -0400

    second commit, still not quite right...

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 $(() => {
 
-    $("#available").on("click", (test) => {
-      $("#container").css("display", "none");
-       $("#hiddenForm").css("display", "block");
-       // $("#hiddenForm").css("display", "show")
+let getTable;
+    $(".available").on("click", (test) => {
+    $("#container").css("display", "none");
+    $("#hiddenForm").css("display", "block");
+       // $("#hiddenForm").css("display", "show");
+
+   // let getTable;
+     // $(".available").on("click", reserveTable);
+     $("#btnSave").on("click", saveTable);
+     $("#btnClose").on("click", close;
+
+     function reserveTable() {
+       if ($(this).hasClass('reserved')) {
+           return;
+       }
+       else {
+         let headCount = $(this).text();
+         getTable = $(this);
+         console.log(getTable);
+         $("#container").css("display", "none");
+         $("#hiddenForm").css("display", "block");
+         $("#table-number").text(headCount);
+       }
+   }
 
 
-     });
+       function saveTable() {
+          getTable.removeClass("available");
+          getTable.addClass("reserved");
+          $("#container").css("display", "block");
+          $("#hiddenForm").css("display", "none");
+
+        }
+
+        function close() {
+            $("#container").css("display", "block");
+            $("#hiddenForm").css("display", "none");
+        }
+
+
+    });
  });
