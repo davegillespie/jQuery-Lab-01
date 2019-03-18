e33bf0d578e32656112f9c6c583f4540e3128f2f
dave <d.gillespie221@gmail.com>
2019-03-18 15:26:13 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(() => {
+
+    $("#available").on("click", (test) => {
+      $("#container").css("display", "none");
+       $("#hiddenForm").css("display", "block");
+       // $("#hiddenForm").css("display", "show")
+
+
+     });
+ });
